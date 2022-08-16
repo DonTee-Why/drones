ac877a363965bd667bc0ca4afea1e049280bf895
Timilehin Olusegun <timiddon97@gmail.com>
2022-08-16 21:16:00 +0100

    Added medication repository

---
+++ drones/src/main/java/com/app/drones/repository/MedicationRepository.java
@@ -0,0 +1,10 @@
+package com.app.drones.repository;
+
+import com.app.drones.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MedicationRepository extends JpaRepository<Medication, String> {
+//    Medication findById(String serialNumber);
+}
