afac32d5e88559791eeea8ebb44fd40016a6926f
Timilehin Olusegun <timiddon97@gmail.com>
2022-08-17 15:16:00 +0100

    Added exception handler

---
+++ drones/src/main/java/com/app/drones/advice/ApiExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.app.drones.advice;
+
+import com.app.drones.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidArgument(MethodArgumentNotValidException exception){
+        Map<String, Object> errorResponse = new HashMap<>();
+        Map<String, String> fieldErrors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+
+        errorResponse.put("message", "Invalid input");
+        errorResponse.put("errors", fieldErrors);
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFound(ResourceNotFoundException exception){
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("error", exception.getMessage());
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<Map<String, String>> handleForbidden(HttpClientErrorException.Forbidden exception){
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", exception.getMessage());
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
